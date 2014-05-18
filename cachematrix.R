0e3da1ca77539bb72144e0271356e71fe0693237
mathans <thegreatmathan@yahoo.co.in>
2014-05-19 01:23:44 +0300

    Done
    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
+## Caching Inverse of a Matrix to avoid recalculation
 ## functions do
 
-## Write a short comment describing this function
+## Encapsulation for Matrix Variable with set get setinverse getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im<- NULL
+  set<-function(y){
+    x<<-y
+    im<-NULL
+  }
+  get<- function() x
+  setinverse<-function(solved) im<-- solved
+  getinverse<-function() im
+  list(set=set, get = get, setinverse= setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Validate Is the Inverse is calculated already if so without recalculation return it else calculate it
 
 cacheSolve <- function(x, ...) {
+  im<- x$getinverse()
+  if(!is.null(im)){
+    message("Getting Cached Inverse")
+    return(im)
+  }
+  data<-x$get()
+  im<-solve(data, ...)
+  x$setinverse(im)
+  im
         ## Return a matrix that is the inverse of 'x'
 }
